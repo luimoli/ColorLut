edc7153a223074f437787a110455b0dcded96613
luimoli <mengmeng.liu@smartmore.com>
2023-09-06 11:26:45 +0800

    feat: patch index gradient

--- lut_apply.py
+++ lut_apply.py
@@ -12,9 +12,6 @@ from tqdm import tqdm
 
 
 if __name__ =='__main__':
-    # img_path = Path('./test/kit-suman-5mcnzeSHFvE-unsplash.jpg')
-    # img_path = Path('./image/marvin-kuhn-uHrRgJKPPAk-unsplash.jpg')
-    # img_path = Path('./data_9/0.png')
 
     lut_table_path = 'lut\lut_9.txt'
     lut = np.loadtxt(lut_table_path, delimiter='\t')
@@ -26,9 +23,9 @@ if __name__ =='__main__':
         # img = cv2.resize(img, None, fx=0.25, fy=0.25)
         img = img[:, :, ::-1].copy() / 255.
 
-        # res = trilinear_interpolation(lut, img) * 255.
+        # res = trilinear_interpolation(lut, img)
         res = nearest_interpolation(lut, img)
-
+        res = np.clip(res, 0, 1)
         res = res[:, :, ::-1].copy() * 255.
-        cv2.imwrite(str(img_path.parent.parent / 'test' / f'{img_path.stem}_nearest{img_path.suffix}'), res)
+        cv2.imwrite(str(img_path.parent.parent / 'test' / f'nearest_{img_path.stem}{img_path.suffix}'), res)
     
--- lut_packing.py
+++ lut_packing.py
@@ -8,13 +8,13 @@ def get_point_index(upper_left, bottom_right, rows, cols):
     """for a single img, get color patches's indexes.
 
     Args:
-        upper_left (_type_): _description_
-        bottom_right (_type_): _description_
-        rows (_type_): _description_
-        cols (_type_): _description_
+        upper_left (tuple): (h,w)
+        bottom_right (tuple): (h,w)
+        rows (int): 
+        cols (int): 
 
     Returns:
-        _type_: _description_
+        list: list contains indexs with tuple format.
     """
     upper_left_h, upper_left_w = upper_left
     bottom_right_h, bottom_right_w = bottom_right
@@ -27,14 +27,52 @@ def get_point_index(upper_left, bottom_right, rows, cols):
         for j in range(cols):
             point_h = upper_left_h + i * height_interval
             point_w = upper_left_w + j * width_interval
-            point_list.append((point_h, point_w))
+            point_list.append((int(point_h), int(point_w)))
+
+    return point_list
+
+def get_point_index_gradient(upper_left, upper_right, bottom_left, rows, cols):
+    """for a single img, get color patches's indexes.
+
+    Args:
+        upper_left (_type_): _description_
+        bottom_right (_type_): _description_
+        rows (_type_): _description_
+        cols (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    y1, x1 = upper_left
+    y2, x2 = upper_right
+    y3, x3 = bottom_left
+    # y4, x4 = bottom_right
+
+    kw = (y2 - y1) / (x2 - x1)
+    # bw = y1 - kw*x1
+    kh = (y3 - y1) / (x3 - x1)
+    bh = y1 - kh*x1
+
+    width_interval = (x2 - x1) // (cols - 1)
+    height_interval = (y3 - y1) // (rows - 1)
+    point_list = []
+    for i in range(rows):
+        for j in range(cols):
+            y_start = y1 + i * height_interval
+            x_start = (y_start-bh)/kh
+
+            b_cur_w = y_start - kw*x_start
+            point_w = x_start + j * width_interval
+            point_h = kw * point_w + b_cur_w
+
+            point_list.append((int(point_h), int(point_w)))
 
     return point_list
 
 def check_point_index(img_path, save_path, point_list):
     img = cv2.imread(img_path)
     for point in point_list:
-        cv2.circle(img, (point[1],point[0]), 10, (255, 0, 0), -1)
+        cv2.circle(img, (point[1],point[0]), 15, (0, 0, 255), -1)
     cv2.imwrite(save_path, img)
 
 
@@ -83,11 +121,16 @@ def get_color(img_root, point_list, cut_range, original_length):
 
 
 if __name__ ==  '__main__':
-    point_list = get_point_index((1967, 1804), (5023, 7368), 6, 8)
+    # manually
+    # point_list = get_point_index((1967, 1804), (5023, 7368), 6, 8)
+    point_list = get_point_index_gradient((1873, 1795), (2002, 7461), (4925, 1750), 6, 8)
+    # print(point_list)
+    check_point_index(img_path='patch_display\img_9\DSC01782.JPG', save_path='./test.jpg', point_list=point_list)
 
-    img_root = f'./img_9'
-    lut = get_color(img_root, point_list, 200, 9*9*9)
+    # # generate lut
+    # img_root = f'./img_9'
+    # lut = get_color(img_root, point_list, 200, 9*9*9)
 
-    # save lut
-    lut = np.array(lut)
-    np.savetxt('./lut/lut_9_255.txt', lut, fmt='%.6f', delimiter='\t')
+    # # save lut
+    # lut = np.array(lut)
+    # np.savetxt('./lut/lut_9_255.txt', lut, fmt='%.6f', delimiter='\t')
