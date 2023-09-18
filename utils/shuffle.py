16990454db95fcfcfa21dbf5ead8e65639e3616b
luimoli <mengmeng.liu@smartmore.com>
2023-09-18 17:09:02 +0800

    feat: add shuffle

---
+++ utils/shuffle.py
@@ -0,0 +1,27 @@
+import random
+
+
+def generate_shuffle():
+    pass
+
+def restore_shuffle():
+    pass
+
+# 创建索引数组
+# index_arr = list(range(len(arr1)))
+
+
+# random.shuffle(index_arr)
+
+# 输出打乱后的索引数组
+# print(index_arr)
+
+
+# 假设第二个数组为arr2
+arr2 = ['a', 'b', 'c', 'd', 'e']
+
+# 根据索引数组对arr2进行重新排序
+# arr2_shuffled = [arr2[i] for i in index_arr]
+
+# 输出打乱后的第二个数组
+# print(arr2_shuffled)
